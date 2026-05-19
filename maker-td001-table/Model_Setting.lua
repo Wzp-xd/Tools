1a5fe6da0ea503278fd58bdfa6f7b82be315045d
wzp <weizhongping@xd.com>
2026-05-19 17:08:51 +0800

    2026-05-19 17:08:51

--- maker-td001-table/Model_Setting.lua
+++ maker-td001-table/Model_Setting.lua
@@ -12,11 +12,101 @@ local Data = {
     Scale = 0.25,
     Sprite = [[image/tower_marauder_20260519024244.png]],
 },
+['医疗兵'] = {
+    ID = "医疗兵",
+    Scale = 0.2,
+    Sprite = [[Textures/towers/tower_firebat.png]],
+},
 ['哥布林1'] = {
     ID = "哥布林1",
     Key = "enemy_goblin_recruit",
     Scale = 0.2,
     Sprite = [[image/enemy_goblin_recruit_20260519024400.png]],
+},
+['哥布林10'] = {
+    ID = "哥布林10",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_shadow_mage.png]],
+},
+['哥布林11'] = {
+    ID = "哥布林11",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_desert_ranger.png]],
+},
+['哥布林12'] = {
+    ID = "哥布林12",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_jungle_hunter.png]],
+},
+['哥布林13'] = {
+    ID = "哥布林13",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_lava_warrior.png]],
+},
+['哥布林14'] = {
+    ID = "哥布林14",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_night_assassin.png]],
+},
+['哥布林15'] = {
+    ID = "哥布林15",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_toxic_shaman.png]],
+},
+['哥布林2'] = {
+    ID = "哥布林2",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/goblin_warrior.png]],
+},
+['哥布林3'] = {
+    ID = "哥布林3",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/goblin_shield.png]],
+},
+['哥布林4'] = {
+    ID = "哥布林4",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/skeleton_soldier.png]],
+},
+['哥布林5'] = {
+    ID = "哥布林5",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_goblin_recruit.png]],
+},
+['哥布林6'] = {
+    ID = "哥布林6",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_goblin_warrior.png]],
+},
+['哥布林7'] = {
+    ID = "哥布林7",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_goblin_chief.png]],
+},
+['哥布林8'] = {
+    ID = "哥布林8",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_skeleton_soldier.png]],
+},
+['哥布林9'] = {
+    ID = "哥布林9",
+    Scale = 0.2,
+    Sprite = [[Textures/enemies/enemy_frost_knight.png]],
+},
+['坦克'] = {
+    ID = "坦克",
+    Scale = 0.3,
+    Sprite = [[Textures/towers/tower_goliath.png]],
+},
+['大和'] = {
+    ID = "大和",
+    Scale = 0.5,
+    Sprite = [[Textures/towers/tower_yamato.png]],
+},
+['女妖'] = {
+    ID = "女妖",
+    Scale = 0.3,
+    Sprite = [[Textures/towers/tower_viking.png]],
 },
 ['机枪兵'] = {
     ID = "机枪兵",
@@ -24,6 +114,26 @@ local Data = {
     Offset = {0, 0},
     Scale = 0.2,
     Sprite = [[image/tower_marine_20260519024245.png]],
+},
+['歌利亚'] = {
+    ID = "歌利亚",
+    Scale = 0.3,
+    Sprite = [[Textures/towers/tower_medic.png]],
+},
+['火蝠'] = {
+    ID = "火蝠",
+    Scale = 0.25,
+    Sprite = [[Textures/towers/tower_tank.png]],
+},
+['维京'] = {
+    ID = "维京",
+    Scale = 0.3,
+    Sprite = [[Textures/towers/tower_thor.png]],
+},
+['雷神'] = {
+    ID = "雷神",
+    Scale = 0.4,
+    Sprite = [[Textures/towers/tower_banshee.png]],
 },
 }
 return Data

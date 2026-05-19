a537049c1f5076a7426253783ae54ae22fb52508
wzp <weizhongping@xd.com>
2026-05-19 11:10:40 +0800

    2026-05-19 11:10:39

--- maker-td001-table/Model_Setting.lua
+++ maker-td001-table/Model_Setting.lua
@@ -10,17 +10,20 @@ local Data = {
     ID = "劫掠者",
     Key = "tower_marauder",
     Scale = 1,
+    Sprite = [[Textures/towers/tower_marauder.png]],
 },
 ['哥布林1'] = {
     ID = "哥布林1",
     Key = "enemy_goblin_recruit",
     Scale = 1,
+    Sprite = [[Textures/enemies/goblin_recruit.png]],
 },
 ['机枪兵'] = {
     ID = "机枪兵",
     Key = "tower_marine",
     Offset = {0, 0},
     Scale = 1,
+    Sprite = [[Textures/towers/tower_marine.png]],
 },
 }
 return Data

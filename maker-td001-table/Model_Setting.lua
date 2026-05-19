74cb1c578c90edcd63167bb2ca87d5f1531e6d50
wzp <weizhongping@xd.com>
2026-05-19 11:40:24 +0800

    2026-05-19 11:40:23

--- maker-td001-table/Model_Setting.lua
+++ maker-td001-table/Model_Setting.lua
@@ -10,20 +10,20 @@ local Data = {
     ID = "劫掠者",
     Key = "tower_marauder",
     Scale = 1,
-    Sprite = [[image\tower_marauder_20260519024244.png]],
+    Sprite = [[assets/image/tower_marauder_20260519024244.png]],
 },
 ['哥布林1'] = {
     ID = "哥布林1",
     Key = "enemy_goblin_recruit",
     Scale = 1,
-    Sprite = [[image\enemy_goblin_recruit_20260519024400.png]],
+    Sprite = [[assets/image/enemy_goblin_recruit_20260519024400.png]],
 },
 ['机枪兵'] = {
     ID = "机枪兵",
     Key = "tower_marine",
     Offset = {0, 0},
     Scale = 1,
-    Sprite = [[image\tower_marine_20260519024245.png]],
+    Sprite = [[assets/image/tower_marine_20260519024245.png]],
 },
 }
 return Data
